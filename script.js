8cf2808de2369cfdd075741340e19a4248a94ff5
Tsuji-tomonori33 <tsuji-tomonori@kago.ed.jp>
2025-10-02 15:38:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+// index.htmlでスタンプ状態を反映
+document.addEventListener("DOMContentLoaded", () => {
+  let complete = true;
+  for (let i = 1; i <= 5; i++) {
+    const stamp = document.getElementById(`stamp${i}`);
+    if (stamp) {
+      if (localStorage.getItem(`stamp${i}`) === "get") {
+        stamp.src = `images/stamps/stamp${i}_got.png`;
+      } else {
+        complete = false;
+      }
+    }
+  }
+  if (complete) {
+    document.getElementById("complete-message").style.display = "block";
+  }
+});
